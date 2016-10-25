53bf55f3d47665fc86634ee9eeb691ec0a5aa4e3
taylorpagesmith <taylorpagesmith@75-112-94-220.res.bhn.net>
2016-10-25 19:01:49 -0400

    Beggining to Add Spring MVC Support

---
+++ GolfApp/src/hello/GreetingController.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+package hello;
+
+/**
+ *
+ * @author taylorpagesmith
+ */
+@Controller
+public class GreetingController {
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+}
